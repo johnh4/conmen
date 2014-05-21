a748ca3a19d217ff20a0949c12fd74bc9c8b8123
johnh4 <jehowl4@gmail.com>
2014-05-21 11:43:03 -0400

    Redistogo url config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  ENV["REDISTOGO_URL"] = 'redis://localhost:6379'
+  #ENV["REDISTOGO_URL"] = 'redis://localhost:6379'
 	
 	# Settings specified here will take precedence over those in config/application.rb.
 
