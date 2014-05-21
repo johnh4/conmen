050d61e4f131d753273f91ca5cf2746ea26f1bf0
johnh4 <jehowl4@gmail.com>
2014-05-21 12:07:39 -0400

    Redis uri config debug.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
 uri = URI.parse(Rails.env.poduction? ? ENV["REDISTOGO_URL"] : "redis://localhost:6379/")
+puts "redis uri: #{uri}"
 $redis = Redis.new(:host => uri.host, :port => uri.port, 
 									 :password => uri.password)
