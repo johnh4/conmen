3171df701ea4ee2cd3ed7a6f4c03a6fc7c282497
johnh4 <jehowl4@gmail.com>
2014-05-21 12:17:19 -0400

    Controller using proper redis instance.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,7 @@ class HomeController < ApplicationController
 	
 	def events
 		response.headers["Content-Type"] = "text/event-stream"
-		redis = Redis.new
-		redis.subscribe('tweets.create') do |on|
+		$redis.subscribe('tweets.create') do |on|
 			puts 'in tweets create sub on'
 			on.message do |event, data|
 				#puts "in on.message, event: #{event}, data: #{data}"
@@ -14,7 +13,7 @@ class HomeController < ApplicationController
 		rescue IOError
 			logger.info "Stream closed"
 		ensure
-			redis.quit
+			$redis.quit
 			response.stream.close
 	end
 
